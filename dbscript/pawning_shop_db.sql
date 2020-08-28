518293cc5285d5ef304e9ac51c58382cb7fd93bb
Malaka <malakasilva43@gmail.com>
2020-08-28 16:36:34 +0530

    Add db script

---
+++ dbscript/pawning_shop_db.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 28, 2020 at 11:05 AM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `pawning_shop_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE IF NOT EXISTS `customers` (
+  `nic` varchar(15) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `contact_no` int(11) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`nic`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE IF NOT EXISTS `employee` (
+  `empid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`empid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE IF NOT EXISTS `item` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) NOT NULL,
+  `weight` double NOT NULL,
+  `value` double NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `nic` varchar(15) NOT NULL,
+  PRIMARY KEY (`item_id`,`nic`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan`
+--
+
+CREATE TABLE IF NOT EXISTS `loan` (
+  `loan_id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` double NOT NULL,
+  `duedate` datetime NOT NULL,
+  `billdate` datetime NOT NULL,
+  `emp_id` int(11) NOT NULL,
+  `nic` varchar(15) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  PRIMARY KEY (`loan_id`,`nic`,`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE IF NOT EXISTS `payment` (
+  `payment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) NOT NULL,
+  `amount` double NOT NULL,
+  `emp_id` int(11) NOT NULL,
+  `nic` varchar(15) NOT NULL,
+  `loan_id` int(11) NOT NULL,
+  PRIMARY KEY (`payment_id`,`nic`,`loan_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
